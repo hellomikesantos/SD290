bd1a7c99a66ab3ae7bf5d2327e8aa3282d33a960
micsant <100429563+micsant@users.noreply.github.com>
2022-06-07 09:56:50 -0500

    Lab4

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,54 +1,59 @@
-bool IsPalindrome(string testString)
-{
-    int boolChecker = 0;
-    for (int i = 0; i < testString.Length; i++)
-    {
-        foreach (char c in testString)
-        {
-            if(c == ' ')
-            {
-                break;
-            }
-        }
-        for(int j = testString.Length - 1; j > (testString.Length - 1) / 2; j--)
-        {
-  
-            if(testString[j] == testString[i])
-            {
-                Console.WriteLine(boolChecker);
-                boolChecker++;
-            }
+List<int> myIntList = new List<int> { 1, 5, 3 };
+List<int> myIntList2 = new List<int> { 9, 7, 3, -2 };
+List<int> myIntList3 = new List<int> { 2, 1, 2 };
 
-        }   
+int GetHighestValueInList(List<int> intList)
+{
+    int highest = intList[0];
+    for (int i = 1; i < intList.Count; i++)
+    {
+        if (intList[i] > highest)
+        {
+            highest = intList[i];
+        }
     }
-    return boolChecker == (testString.Length - 1);
+    return highest;
 }
 
-Console.WriteLine("Enter a word / phrase");
-
-if(IsPalindrome(Console.ReadLine()))
+List<int> MaxNumbersInLists()
 {
-    Console.WriteLine("This is Palindrome");
+    List<int> highestInts = new List<int> { };
+    highestInts.Add(GetHighestValueInList(myIntList));
+    highestInts.Add(GetHighestValueInList(myIntList2));
+    highestInts.Add(GetHighestValueInList(myIntList3));
+
+    Console.WriteLine($"List 1 has a maximum of {GetHighestValueInList(myIntList)}. List 2 has a maximum of {GetHighestValueInList(myIntList2)}." +
+        $" List 3 has a maximum of {GetHighestValueInList(myIntList3)}");
+    return highestInts;
 };
 
+MaxNumbersInLists();
 
-char[] DuplicateCharacters(string testString)
+List<int> class1 = new List<int> { 85, 92, 67, 94, 94 };
+List<int> class2 = new List<int> { 50, 60, 57, 99 };
+List<int> class3 = new List<int> { 95 };
+
+String HighestGrade()
 {
-    char[] duplicatedChars = new char[testString.Length];
-    List<char> charList = duplicatedChars.ToList();
-    for (int i = 0; i < testString.Length; i++)
+    List<int> highGrades = new List<int> { };
+    highGrades.Add(GetHighestValueInList(class1));
+    highGrades.Add(GetHighestValueInList(class2));
+    highGrades.Add(GetHighestValueInList(class3));
+    int highestGrade = GetHighestValueInList(highGrades);
+    int highest = highGrades[0];
+    int classNum = 0;
+    for (int i = 1; i < highGrades.Count; i++)
     {
-        for(int j = 1; j < testString.Length; j++)
+        if (highGrades[i] > highest)
         {
-            if (testString[i] == testString[j])
-            {
+            highest = highGrades[i];
+            classNum = i;
+        }
+    }
+    Console.WriteLine($"The highest grade is {highest}. This grade was found in class(es) {classNum}");
+    return $"The highest grade is {highest}. This grade was found in class(es) {classNum}";
+}
 
-                charList.Add(testString[j]);
-            };
-        };
-    };
-    Console.WriteLine(charList);
-    return duplicatedChars;
-};
+HighestGrade();
+Console.WriteLine("Sample WriteLine");
 
-DuplicateCharacters(Console.ReadLine());
